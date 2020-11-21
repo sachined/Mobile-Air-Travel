8e1447b8c5f46cabd0a4657013a143ec8e610998
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-11-21 02:18:10 -0800

    Update World_Planes.cpp
    Defined a function for Building class, changed constructors to reflect data member inclusion (airName)
    
    Added noexcept to the move constructor (this might be unnecessary or causing a bug)
    
    Renamed few functions to remove confusion between cityName and airName. 
    
    freeAirSpace function was created to point a pointer to null to prevent memory leak. (STILL NOT WORKING, when copied/moved airPort pointers are involved, issues arise)
    
    sort functions are defined
    
    Note: there are commented out code that might or might not be used later

--- World_Planes.cpp
+++ World_Planes.cpp
@@ -1,6 +1,11 @@
 #include "World_Planes.h"
 #include <utility> // std::move
 
+void Building::Name()
+{
+	std::cout << "I am a BUILDING!\n\n";
+}
+
 /* AIRPORT ===============================================
 
 This class will store airport info, such as city and code
@@ -9,26 +14,20 @@ Can be used in mapQueue (a vector of pointers)
 */
 
 // Default Constructor
-airPort::airPort(): cityName("Phoenix"), airCode("PHX")  {
-	/*setAirName("Phoenix");
-	setAirCode("PHX");*/
-}
+airPort::airPort(): airName("DogWood Shining"), cityName("Phoenicia"), airCode("PNA")  {}
 
 // Constructor
-airPort::airPort(std::string placeName, std::string placeCode) {
-	setAirName(placeName);
-	setAirCode(placeCode);
-}
+airPort::airPort(std::string placeName, std::string placeCity, std::string placeCode) : airName(placeName), cityName(placeCity), airCode(placeCode) {}
 
 // Destructor
 airPort::~airPort() {
-	setAirName("");
-	setAirCode("");
+	/*setCityName("");
+	setAirCode("");*/
 	displayNullAir();
 }
 
 // Copy Constructor
-airPort::airPort(const airPort& other) : cityName(other.cityName), airCode(other.airCode)
+airPort::airPort(const airPort& other) : airName(other.airName), cityName(other.cityName), airCode(other.airCode)
 {
 	std::cout << "Copy constructor.." << std::endl;
 	//airPort* ptr = new airPort(obj.getCityName(), obj.getCode());
@@ -37,13 +36,16 @@ airPort::airPort(const airPort& other) : cityName(other.cityName), airCode(other
 }
 
 // Move Constructor (This will move the pointer of other to this and then makes other nullptr)
-airPort::airPort(airPort&& other) {
+airPort::airPort(airPort&& other) noexcept {
 	std::cout << "Move constructor.." << std::endl;
 	*this = std::move(other);
+	other.airCode = nullptr;
+	other.cityName = nullptr;
+	other.airName = nullptr;
 }
 
 // Copy Assignment operator
-airPort& airPort::operator = (const airPort& other) {
+airPort& airPort::operator=(const airPort& other) {
 	if (this == &other)
 		return *this;
 	PlaneNumber = other.PlaneNumber;
@@ -51,26 +53,37 @@ airPort& airPort::operator = (const airPort& other) {
 }
 
 // Move Assignment operator
-airPort& airPort::operator = (airPort&& other)	{
+airPort& airPort::operator=(airPort&& other)	{
 	this->PlaneNumber = std::move(other.PlaneNumber);
+	//this->cityName = std::move(other.cityName);
+	//this->airCode = std::move(other.airCode);
+	//other.PlaneNumber = nullptr;
 	return *this;
 }
 
-
 // This will display the plane number through a function (airPort())
-int airPort::operator()()const {	return this->PlaneNumber;	}
+int airPort::operator()() const {	return this->PlaneNumber;	}
 
+// ---------------------------------------------------
+// Setting name of the airport
+void airPort::setName(std::string name) {	airName = name;	}
+
+// Show the name of the airport
+void airPort::getName()	{	std::cout << GetAir();	}
+
+std::string airPort::GetAir() const	{	return airName;	}
+// ---------------------------------------------------
 // +++++++++++++++++++++++++++++++++++++++++++++++++++
+// Setting name of city where airport is located
+void airPort::setCityName(std::string place) {	cityName = place;	}
+
 // Getting name of city where airport is located
-void airPort::getAirName() {
-	std::cout << "Airport located at " << getCityName() << '\n';
+void airPort::getCityName() {
+	std::cout << GetCityName();
 }
 
-void airPort::setAirName(std::string place) {	cityName = place;	}
-
-std::string airPort::getCityName() const {	return cityName;	}
+std::string airPort::GetCityName() const {	return cityName;	}
 // +++++++++++++++++++++++++++++++++++++++++++++++++++
-
 int airPort::PlaneNumber = 4423;
 
 //static function
@@ -78,53 +91,92 @@ void airPort::printPlane() {
 	std::cout << "Plane # is " << airPort::PlaneNumber << '\n';
 }
 
-
 //Operations regarding airCode
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+void airPort::setAirCode(std::string code) {	airCode = code;	}
+
 void airPort::getAirCode() {
-	std::cout << "Code: " << getCode() << std::endl;
+	std::cout << GetCode();
 }
 
-void airPort::setAirCode(std::string code) {
-	airCode = code;
-}
-
-std::string airPort::getCode() const {	return airCode;	}
+std::string airPort::GetCode() const {	return airCode;	}
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
 void airPort::displayAir() {
 	std::cout << "\nAirport currently:\n";
-	getAirName();
+	getName();
+	getCityName();
 	getAirCode();
 	std::cout << "\\\n";
 }
 
-// Only used for the destructor (for future memory purpose)
-void airPort::displayNullAir() {
-	std::cout << "This airport does not exist\n" << std::endl;
+void airPort::freeAirSpace() {
+	setAirCode("");
+	setCityName("");
+	setName("");
+	delete this;
 }
 
-/* MAP QUEUE 
+// Only used for the destructor (for future memory purpose)
+void airPort::displayNullAir() {
+	std::cout << "The airport in " << cityName << " does not exist\n" << std::endl;
+}
+
+// ==============================================================
+/* MAP QUEUE ****************************************************
 
 This will store a list of airports that can be used
 */
 
 mapQueue::mapQueue()	{
-	std::cout << "A map is being created...\n";
+	std::cout << "A map is being created...\n\n";
 	size = 0;
 }
 
 mapQueue::~mapQueue() {
-	cities.clear();
+	std::cout << '\n';
+	/*cities.erase(cities.begin(), cities.begin() + cities.size());*/
+	for (auto & elem : cities) {
+		if (elem == nullptr) {
+			delete elem;
+		}
+		else
+		{
+			elem->~airPort();
+			elem->freeAirSpace();
+			elem = nullptr;
+			delete elem;
+		}	
+	}
+	cities.shrink_to_fit();
 	std::cout << "The map is being closed...\n";
 	size = 0;
 }
 
+void mapQueue::display()	{
+	std::vector<airPort*>::iterator it;
+
+	std::cout << '\n';
+
+	for (it = cities.begin(); it != cities.end(); it++) {
+		(*it)->getName();
+		std::cout << " airport @ ";
+		(*it)->getCityName();
+		std::cout << " (";
+		(*it)->getAirCode();
+		std::cout << ")\n";
+	}
+	std::cout << '\n';
+}
+
 // Adding to the vector
 void mapQueue::add(airPort** newPlace) {
-	cities.push_back(*newPlace);
-	std::cout << (*(newPlace))->getCityName() << " has been added!\n";
+	//currentAir = new airPort();
+	currentAir = *newPlace;
+	cities.push_back(currentAir);
+	currentAir->getCityName();
+	std::cout << " has been added!\n";
 	size++;
+	currentAir = nullptr;
 }
 
 
@@ -132,50 +184,87 @@ void mapQueue::add(airPort** newPlace) {
 // still not completely BUGS FREE
 
 /* Towards the end of the function, the vector is not properly populated
-
 	For example, if there are 3 airports and one is removed,
-
 	3rd airport has two places occupied...
-
 	This is still an oncoming fix
-
 	PLEASE BE CAREFUL!
 
+	11/20/20 Fixed 
 
 */
 
 void mapQueue::remove(airPort** oldPlace)	{
-		currentAir = *oldPlace;
 
-		bool found = false;
-		unsigned position = 0;
+	bool found = false;
+	unsigned pos = 0;
+	
 
-		// iterate over all elements in vector
-		for (auto & elem : cities)
+	// iterate over all elements in vector
+	for (auto & elem : cities)	{
+		if (elem == *oldPlace && !found)
 		{
-			if (elem == currentAir)
-			{
-				found = true;
-				std::cout << currentAir->getCityName() <<" has been found!" << std::endl;
-				break;
-			}
-			else if (found == false)
-			{
-				position++;
-			}
+			found = true;
+			std::cout << elem->GetCityName() <<" found!\n";
+			elem = nullptr;
+			delete elem;
+			std::cout << "This airport is deleted\n";
+			//currentAir = nullptr;
+			break;
 		}
-		if (found) {
-			currentAir->displayAir();
-
-		}
-		for (unsigned i = position; i < cities.size(); i++)
-		{
-			if (position == cities.size() - 1) {
-
-				cities.resize(position);
-				break;
-			}
-			cities[position] = cities[position + 1];
-		}
-
+		else
+			pos++;
 	}
+	for (unsigned i = pos; i < cities.size(); i++)	{
+		if (i == cities.size() - 1) 
+		{
+			cities.resize(i);
+			size = i;
+			cities.shrink_to_fit();
+			break;
+		}
+			cities[i] = cities[i + 1];
+	}
+	*oldPlace = nullptr;
+	delete *oldPlace;
+}
+
+// Sorting functions @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Sort by city 
+void mapQueue::sortByCity(bool flag)	{
+	if (flag) {
+		std::sort(cities.begin(), cities.end(), [](airPort* a, airPort* b) 
+		{	return a->GetCityName() < b->GetCityName(); });
+	}
+	else
+	{
+		std::sort(cities.begin(), cities.end(), [](airPort* a, airPort* b) 
+		{	return a->GetCityName() > b->GetCityName(); });
+	} 
+}
+// Sort by air code
+void mapQueue::sortByCode(bool flag)	{
+	if (flag) {
+		std::sort(cities.begin(), cities.end(), [](airPort* a, airPort* b) 
+		{	return a->GetCode() < b->GetCode(); });
+	}
+	else
+	{
+		std::sort(cities.begin(), cities.end(), [](airPort* a, airPort* b)
+		{	return a->GetCode() > b->GetCode(); });
+	}
+}
+// Sort by airport name
+void mapQueue::sortByName(bool flag)
+{
+	if (flag) {
+		std::sort(cities.begin(), cities.end(), [](airPort* a, airPort* b)
+		{	return a->GetAir() < b->GetAir(); });
+	}
+	else
+	{
+		std::sort(cities.begin(), cities.end(), [](airPort* a, airPort* b)
+		{	return a->GetAir() > b->GetAir(); });
+	}
+}
+
+// **************************************************************************
